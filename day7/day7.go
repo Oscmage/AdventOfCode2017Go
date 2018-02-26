17b9d40e298d29c6cd8478abce143ac7e4988401
Oscar Evertsson <oscar.evertsson@live.com>
2018-02-26 09:20:12 +0100

    Removed duplicate usage of child name.

--- day7/day7.go
+++ day7/day7.go
@@ -9,25 +9,23 @@ import (
 const fileName = "day7/input"
 
 type element struct {
-	name     string
 	val      int
-	children []*element
+	children []string
 }
 
-func parseChildren(line []string, m map[string]*element) []*element {
-	children := make([]*element, 0)
+func parseChildren(line []string, m map[string]*element) []string {
+	children := make([]string, 0)
 	for i := 3; i < len(line); i++ {
 		child := strings.TrimSuffix(line[i], ",")
 		childElem, ok := m[child]
 		if ok {
 			// Child already in map
-			children = append(children, childElem)
+			children = append(children, child)
 		} else {
 			// Child not in map, lets create a one
 			childElem = &element{}
-			childElem.name = child
 			m[child] = childElem
-			children = append(children, childElem)
+			children = append(children, child)
 		}
 	}
 	return children
@@ -52,7 +50,7 @@ func parse() map[string]*element {
 			elem.children = children
 		} else {
 			// Element doesn't exist so lets add it!
-			newElem := &element{name, v, children}
+			newElem := &element{v, children}
 			m[name] = newElem
 		}
 	}
@@ -66,7 +64,7 @@ func FirstPart() string {
 	// Find all children and add them to the childMap
 	for _, element := range m {
 		for _, child := range element.children {
-			childMap[child.name] = true
+			childMap[child] = true
 		}
 	}
 
